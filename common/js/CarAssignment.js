9056b0224412256ffe18e406750904a27caa2f73
kyusooK <rbtn110@uengine.org>
2024-11-28 09:42:27 +0000

    c

--- common/js/CarAssignment.js
+++ common/js/CarAssignment.js
@@ -123,3 +123,23 @@ function save(){
         }     
     }           
 }
+function submitRegisterDrivingLog(data){
+    const registrationId = data.registrationId;
+    fetch(`http://localhost:8088/vehiclePerformances/registerDrivingLog/${registrationId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        alert(error);
+    });
+
+    
+}
